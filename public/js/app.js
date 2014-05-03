2ea9260904c4c1cac08b0ed95fa02ea1a8a7bd4e
vagrant <vagrant@student-dev-box>
2014-05-03 22:28:26 +0000

    added JS to pull current user's profile information after auth. created initial backbone model and view

---
+++ public/js/app.js
@@ -0,0 +1,50 @@
+//on load, authenticate viewer
+function onLinkedInLoad() {
+  IN.Event.on(IN, "auth", onLinkedInAuth);
+}
+ //Runs when the viewer has authenticated
+function onLinkedInAuth() {
+  IN.API.Profile("me").result(function(profiles) {
+  person = profiles.values[0];
+  console.log(person.firstName);
+  
+  var app = {}; //create namespace
+    
+    //create model
+    app.Profile = Backbone.Model.extend({
+      defaults: {
+        first: '',
+        last: '',
+        avatar: '',
+        headline: ''
+      }
+    });
+    //instantiate my model with current user data
+    var myprofile = new app.Profile({
+      first: person.firstName,
+      last: person.lastName,
+      avatar: person.pictureUrl,
+      headline: person.headline
+    });
+
+  //create a view to render 
+  app.ProfileView = Backbone.View.extend({
+    el: $('#container'),
+    template: _.template("<div>"+ myprofile.get('first')+" "+myprofile.get('last')+"</div>"),
+    initialize: function(){
+      this.render();
+    },
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    }
+  });
+
+  var view = new app.ProfileView({model: myprofile});
+  
+  }); 
+
+} //close auth function
+
+
+
